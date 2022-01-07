ebdc3a4b732802de8189b75d691c53e96496c371
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2022-01-08 01:04:30 +0900

    append examples.

--- examples/mkset_examples.v
+++ examples/mkset_examples.v
@@ -10,7 +10,7 @@ Section Classical_Sets_Examples.
   | Orange
   | PineApple
   | Mango.
-  
+
   Definition IsMyFruits (fruits : Fruits) : Prop :=
     match fruits with
     | Apple => True
@@ -19,6 +19,7 @@ Section Classical_Sets_Examples.
     | _ => False
     end.
 
+  Print mkset.
   Definition MyFruits : set Fruits := mkset IsMyFruits.
 
   Goal Apple \in MyFruits.
@@ -29,5 +30,40 @@ Section Classical_Sets_Examples.
     rewrite mksetE.
     done.
   Qed.
-  
+
+  Goal PineApple \in ~`MyFruits.
+  Proof.
+    unfold setC.
+    rewrite in_setE.
+    rewrite mksetE.
+    unfold MyFruits.
+    rewrite mksetE.
+    done.
+  Qed.
+
+  Print nat.
+
+  Fixpoint IsEven(n : nat) : Prop :=
+    match n with
+    | O => True
+    | S O => False
+    | S (S n') => IsEven n'
+    end.
+
+  Goal 2 \in mkset IsEven.
+  Proof.
+    rewrite in_setE.
+    unfold mkset.
+    unfold IsEven.
+    done.
+  Qed.
+
+  Goal 3 \notin mkset IsEven.
+  Proof.
+    rewrite notin_set.
+    unfold mkset.
+    unfold IsEven.
+    apply.
+  Qed.
+
 End Classical_Sets_Examples.
