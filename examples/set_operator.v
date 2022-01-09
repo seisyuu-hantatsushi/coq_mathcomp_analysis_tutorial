f7065c120de53f413f792de9ac9d8a56fc3670fb
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2022-01-09 14:45:45 +0900

    対象差の例.

--- examples/set_operator.v
+++ examples/set_operator.v
@@ -61,6 +61,25 @@ Section Sets_Operator.
       apply setCU.
     Qed.
 
+    Definition setS A B := (A `\` B) `|` (B `\` A).
+
+    Goal forall A B, setS A B = (A `|` B) `\` (A `&` B).
+    Proof. move => A B. by rewrite setDUl !setDIr !setDv set0U setU0. Qed.
+
+    Lemma setCD A B : ~`(A `\` B) = ~`A `|` B.
+    Proof. by rewrite setCI setCK. Qed.
+
+    Goal forall A B, setS A B = setS (~`A)  (~`B).
+    Proof.
+      move => A B.
+      rewrite -(setCK (setS A B)).
+      rewrite setCU !setCD.
+      unfold setS.
+      rewrite -(setCK (~` A `\` ~` B )) -(setCK (~` B `\` ~` A )).
+      rewrite !setCD -setCI !setCK setIC setUC (setUC (~` A) B).
+      reflexivity.
+    Qed.
+
   End UseImplicit.
 
 End Sets_Operator.
