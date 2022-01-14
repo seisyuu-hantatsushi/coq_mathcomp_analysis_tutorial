8e550aac72e524ab40cbdcbd7ae5bf9755d3e6a5
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2022-01-14 23:16:45 +0900

    写像関連の証明を追加.

--- examples/image.v
+++ examples/image.v
@@ -17,5 +17,24 @@ Section Image.
 
   Goal forall f A, A `<=` f @^-1` (f @` A).
   Proof. by apply preimage_image. Qed.
+
+  Goal forall f A B, f @` A `\` f @` B `<=` f @` (A `\` B).
+  Proof.
+    move => f A B y [[a HAa] Hfay HnB].
+    exists a.
+    split;[apply HAa|
+           move => HBa;apply HnB;exists a].
+    apply HBa.
+    apply Hfay.
+      by apply Hfay.
+  Qed.
+
+  Goal forall (T1 T2 T3:Type) (f:T1 -> T2) (g:T2 -> T3), injective f -> injective g -> injective (g \o f).
+  Proof.
+    move => T1 T2 T3 f g Hf Hg s t Hfg.
+    apply: (Hf s t).
+    apply: (Hg (f s) (f t)).
+    apply: Hfg.
+  Qed.
   
 End Image.
